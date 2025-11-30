326451347b5b572d528729ffe72e096a0b633a6a
SchneiderSeba <seba_19_sc@hotmail.com>
2025-11-30 20:58:51 -0300

    Delete Console.log

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_KEY;
 
-console.log('Supabase URL:', supabaseUrl);
-console.log('Supabase Key:', supabaseKey);
+// console.log('Supabase URL:', supabaseUrl);
+// console.log('Supabase Key:', supabaseKey);
 
 export const clientSupaBase = createClient(supabaseUrl, supabaseKey);
